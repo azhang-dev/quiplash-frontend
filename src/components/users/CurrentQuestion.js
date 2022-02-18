750768dc34793b23d30227542b7c1b56a2d64215
Alex King <calicoalix@gmail.com>
2022-02-18 18:02:24 +1100

    routing and game state basics/components

---
+++ src/components/users/CurrentQuestion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CurrentQuestion extends Component {
+    render() {
+        return (
+            <div>
+                <h1>CURRENT QUESTION GOES HERE?</h1>
+            </div>
+        );
+    }
+}
+
+export default CurrentQuestion;
