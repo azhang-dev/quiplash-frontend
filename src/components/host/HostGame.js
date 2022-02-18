61776cc1b5689ac05bf0186ef64968a5d802dadf
“Cassie <cassielee8757@gmail.com>
2022-02-19 01:57:15 +1100

    small stuff

--- src/components/host/HostGame.js
+++ src/components/host/HostGame.js
@@ -1,1 +1,17 @@
 import React from 'react'
+
+class HostGame extends React.Component {
+
+    render(){
+        return(
+            <div>
+
+            <h1>host game starts here</h1>
+
+            </div>
+        )
+    }
+}
+
+export default HostGame
+
