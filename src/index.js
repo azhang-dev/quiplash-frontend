efa493c426e68bd6a1674f48bf84771faea49c34
Alex King <calicoalix@gmail.com>
2022-02-18 18:16:23 +1100

    game routing and state rough draft

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './components/Root';
 import reportWebVitals from './reportWebVitals';
+import App from './App'
 
 ReactDOM.render(
+    // <App />,
     <Root />,
   document.getElementById('root')
 );
