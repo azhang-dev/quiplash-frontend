60eb2eeae3e144404af03f04d2792f8058914f74
Angela Zhang <a_zhang93@hotmail.com>
2022-02-21 17:54:47 +1100

    merged

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ActionCableProvider } from 'react-actioncable-provider';
 import './index.css';
+import Root from './components/Root';
+import App from './App'
 import Test from './Test'
 import { API_WS_ROOT } from './constants';
 
@@ -10,6 +12,8 @@ import { API_WS_ROOT } from './constants';
 ReactDOM.render(
     
   <ActionCableProvider url={API_WS_ROOT}>
+    {/* <App />,
+    <Root />, */}
     <Test />
   </ActionCableProvider>,
   document.getElementById('root')
