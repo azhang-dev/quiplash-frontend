5164d3b8c076e50b4f9f559fb262c87fc3880a9c
Angela Zhang <a_zhang93@hotmail.com>
2022-02-22 12:38:54 +1100

    pulling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ActionCableProvider } from 'react-actioncable-provider';
 import './index.css';
 import Root from './components/Root';
 import App from './App'
 import Test from './Test'
+import { ActionCableProvider } from 'react-actioncable-provider';
 import { API_WS_ROOT } from './constants';
 
 
