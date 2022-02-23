1ec0c564c061f55b6f0c9f9147fd3b83a6581228
“Cassie <cassielee8757@gmail.com>
2022-02-23 22:06:57 +1100

    root css added

--- src/components/host/HostLobby.js
+++ src/components/host/HostLobby.js
@@ -62,7 +62,7 @@ class HostLobby extends Component {
     playersConnection(){
         let connectedPlayers = [];
         for (let i = 0; i < 8; i++){
-            let lobbyStatus = "connected-players"
+            let lobbyStatus = "connected-player"
             let players = this.state.lobbyPlayers[i];
             if (!players) {
                 lobbyStatus += "empty-player-slot";
@@ -109,8 +109,9 @@ class HostLobby extends Component {
        
         return (
             <div className="hostContainer">
-                <h1>Host lobby {this.state.currentLobby.id}</h1>
-                <button onClick = {this.updateUsersInRoom}>UpdateUsersInRoom</button>
+                <h2>Host lobby {this.state.currentLobby.id}</h2>
+                <h3>Go to ---URL--- and enter code: "{this.props.match.params.id}" to join </h3>
+                <button onClick = {this.updateUsersInRoom}>UpdateUsers</button>
 
                 <ActionCableConsumer // THIS IS CHECKING FOR NEW ROOMS 
 
@@ -120,16 +121,16 @@ class HostLobby extends Component {
 
                 </ActionCableConsumer>
 
-                <h2>Host lobby {this.state.currentLobby.id}</h2>
+                
                 {
                 this.state.currentUser.id === this.state.currentLobby.host_id
                 ?
                 //<NewQuestionForm />
-                <button className ="btn btn-outline-secondary" onClick={this.startGame}>Game Start</button>
+                <button onClick={this.startGame}>Game Start</button>
                 :
                 <p>Waiting for game to start...</p>
                 }
-                <p>Go to ---URL--- and enter code: "{this.props.match.params.id}" to join </p>
+               <br/>
                 <div className = "connected-player">{this.playersConnection()}
 
 
