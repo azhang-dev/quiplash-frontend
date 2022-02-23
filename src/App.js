03e6a1035c9b4ad2a4a4914d271245983a2eafda
Angela Zhang <a_zhang93@hotmail.com>
2022-02-23 16:33:32 +1100

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {Route, Link, HashRouter as Router} from 'react-router-dom';
 
 import Login from './components/Login'
 import MyProfile from './components/MyProfile'
-import { API_ROOT } from '../constants';
-// const API_ROOT = 'http://localhost:3000'
+import { API_ROOT } from './constants';
+// const Base_URL = 'http://localhost:3000'
 
 
 export default class App extends React.Component{
