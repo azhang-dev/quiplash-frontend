04e59270dcb41a73920b7164a12f486e481d37cb
Alex King <calicoalix@gmail.com>
2022-02-23 19:26:52 +1100

    fixed cors w luke

--- src/components/NewRoomForm.js
+++ src/components/NewRoomForm.js
@@ -32,18 +32,31 @@ class NewRoomForm extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault()
-    fetch(`${API_ROOT}/rooms`, {
-      method: 'POST',
-      headers: HEADERS,
-      body: JSON.stringify(this.state)
-    });
+    // fetch(`${API_ROOT}/rooms`, {
+    //   method: 'POST',
+    //   headers: HEADERS,
+    //   body: JSON.stringify(this.state)
+    // });
+    let token = "Bearer " + localStorage.getItem("jwt");
+
+    const res = axios.post( `${API_ROOT}/rooms`, 
+    
+      // headers: {
+      //   'Authorization' : token
+      // },
+      this.state, 
+      {headers: {  'Authorization' : token }})
+    .then(res => {
+    })
+    .catch(err => console.warn(err));
+  
     // this.setState({ title: '' });
 
 
     // this.props.goToLobbyPage()
 
 
-    
+
   };
 
   render = () => {
