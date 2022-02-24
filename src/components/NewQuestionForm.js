a130e5e22f009f0b65a016331ab5ac276c313b1c
Angela Zhang <a_zhang93@hotmail.com>
2022-02-24 19:27:08 +1100

    testing post request for questions

--- src/components/NewQuestionForm.js
+++ src/components/NewQuestionForm.js
@@ -26,7 +26,7 @@ export default class NewQuestionForm extends React.Component {
         let token = "Bearer " + localStorage.getItem("jwt");
         console.log('submit clicked');
         try {
-            const res = await axios.post(`${API_ROOT}/questionbanks/createquestions`, this.state,{headers: {  'Authorization' : token }})
+            const res = await axios.post(`${API_ROOT}/questionbanks/createquestions`, this.state,{headers: { 'Authorization' : token }})
             console.log('post newQuestionbank ', res);
         } catch (err) {
             console.log('Error making new question: ', err);
@@ -48,19 +48,7 @@ export default class NewQuestionForm extends React.Component {
                     <label>Default Answer</label>
                     <input type="text" name="answer" placeholder="Add a Default Answer" onChange={this.handleInput}/>
                 </div> */}
-                {/* <div className="form-input-container">
-                    <label>Question 2</label>
-                    <input type="text" name="question2" placeholder="Add your question" onChange={this.handleInput} />
-                </div>
-                <div className="form-input-container">
-                    <label>Question 3</label>
-                    <input type="text" name="question3" placeholder="Add your question" onChange={this.handleInput}  />
-                </div>
-                <div className="form-input-container">
-                    <label>Question 4</label>
-                    <input type="text" name="question4" placeholder="Add your question" onChange={this.handleInput} />
-                </div>
-                 */}
+               
                 <button>Create</button>
             
             </form>
