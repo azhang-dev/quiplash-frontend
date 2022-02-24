2e4a225ef450b25a9076fe08c444d56873c0b17f
“Cassie <cassielee8757@gmail.com>
2022-02-24 18:19:18 +1100

    button function working

--- src/components/host/HostLobby.js
+++ src/components/host/HostLobby.js
@@ -17,7 +17,8 @@ class HostLobby extends Component {
         questionFormVisible: false,
         gameInfo:[],
         checkLobby: '',
-        questionArray: []
+        questionArray: [],
+        selectedQuestion: []
     };
 
 
@@ -138,7 +139,6 @@ class HostLobby extends Component {
         }
     }
 
-    // const [questionFormVisible, setquestionFormVisible] = useState(false)
 
     handleQuestion = () => {
         let token = "Bearer " + localStorage.getItem("jwt");
@@ -150,23 +150,22 @@ class HostLobby extends Component {
             this.setState({questionArray : res.data})
             
         })
-
-        // });
-
-        
-        // console.log('clicked');
-        // this.setState({questionFormVisible: !this.state.questionFormVisible})
     }
 
-    
-    // questionRend
-
-    // }
-    // showQuestion(){
-    //     if(this.state.quesionFormVisible === true){
-
-    //     }
-    // }
+   chooseQuestion = (e) => {
+       console.log('click',  parseInt(e.target.value) );
+       const questionTarget = parseInt(e.target.value)
+       console.log(this.state.questionArray);
+        
+       this.state.questionArray.map((question, index) => {
+           console.log('questionand indes', question, index);
+           if (index === questionTarget){
+               console.log('clicked');
+               this.setState({selectedQuestion: question})
+            }
+       })
+       
+   }
    
     startGame = () => {
         // this.props.sendData()
@@ -234,11 +233,12 @@ class HostLobby extends Component {
                 }
                 <div className="questionContainer">
                     <div className="questionArray">
-                        {this.state.questionArray.map((question) => {
-                            return <div key={question.name}>{question.name}</div>
+                        {this.state.questionArray.map((question, index) => {
+                            return <div key={question.name}>{question.name}<button value={index} onClick={this.chooseQuestion}>Select</button></div>
                         })}
                             
                     </div>
+                    
                 <br/>
                <br/>
                </div>
